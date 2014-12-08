246272911857cdf26c4b67e2a7acbca9239b290a
ilyakomarov <ilya.komarov@epfl.ch>
2014-12-08 11:02:05 +0100

    Added an option to add ntuples. Just put the name of ntuple in falsk_test and the code will automatically sniff all histos and map them on detector

---
+++ Interactive_Map/DefineHistogram.py
@@ -0,0 +1,14 @@
+from ROOT import *
+import os
+
+def GetAPlot(hist,histname):    
+    if os.path.isfile("static/plots/"+histname+".png"):
+        return "plots/"+histname+".png"
+    else:
+        gROOT.ProcessLine(".x lhcbStyle.C")
+        gStyle.SetOptStat("emr")
+        gStyle.SetPadTopMargin(0.06) 
+        c = TCanvas("c","c", 900, 900)
+        hist.Draw()
+        c.SaveAs("static/plots/"+histname+".png")
+        return "plots/"+histname+".png"
--- Interactive_Map/falsk_test.py
+++ Interactive_Map/falsk_test.py
@@ -11,9 +11,23 @@ NameList = pickle.load(f)
 
 tt_d = create_TT()
 it_d = create_IT()
-#f = open('Collection.pkl')
-#IT_hists = pickle.load(f)
-#Add_Histograms(it_d, IT_hists, 'Efficiency_time_dependence')
+f = open('Collection.pkl')
+IT_hists = pickle.load(f)
+
+
+
+hname ='Efficiency_time_dependence'
+Add_Histograms(it_d, IT_hists, hname)
+
+for h in GetHistosFromNT('STTrackMonitor-2012.root'):
+    if h[0] == 'T':
+        f = open(h+".pkl")
+        TT_hists = pickle.load(f)
+        Add_Histograms(tt_d, TT_hists, h)
+    if h[0] == 'I':
+        f = open(h+".pkl")
+        IT_hists = pickle.load(f)
+        Add_Histograms(it_d, IT_hists, h)
 
 app = Flask(__name__)
 
@@ -34,10 +48,10 @@ def Detector(d):
         return render_template('TT.html', tt=tt_d)
     if d in NameList['TTNames']: 
         p_name = Parse_Name(d)
-        return render_template('Sector.html', sec=tt_d[p_name['layer']][p_name['side']][p_name['sector']])
+        return render_template('Sector.html', sec=tt_d[p_name['layer']][p_name['side']][p_name['sector']], histoname = hname)
     if d in NameList['ITNames']: 
         p_name = Parse_Name(d)
-        return render_template('Sector.html', sec=it_d[p_name['station']][p_name['side']][p_name['layer']][p_name['sector']])
+        return render_template('Sector.html', sec=it_d[p_name['station']][p_name['side']][p_name['layer']][p_name['sector']], histoname = hname)
     return redirect(url_for('hello'))
     #return render_template('Sector.html', sec=d)
 
