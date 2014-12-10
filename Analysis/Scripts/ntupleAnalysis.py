486fac982f23283bb135a454ec1f8ad0c3625b8d
elena <elena.graverini@gmail.com>
2014-12-10 16:25:17 +0100

    Now using TGraphs. Added support for HitsOnTrack and EveryHit samples.

--- Analysis/Scripts/ntupleAnalysis.py
+++ Analysis/Scripts/ntupleAnalysis.py
@@ -4,6 +4,8 @@ import ROOT as r
 from conf import CreateDetectors
 from conf.STChannelID import *
 
+r.gROOT.ProcessLine('.x tools/lhcbStyle.C')
+
 def get_sector(chanID):
     return STNames().uniqueSectorName(STChannelID(int(chanID)))
 
@@ -30,8 +32,15 @@ if __name__ == '__main__':
         print 'Sample usage: python ntupleAnalysis.py TT ../RootFiles/EveryHit/runs131973-133785-end2012.root'
         print 'Exiting now...'
         sys.exit(0)
-    flatDetector = flatten(detector)
     inputFile = str(sys.argv[2])
+    if 'EveryHit' in inputFile:
+        datatype = 'EveryHit'
+    elif 'HitsOnTrack' in inputFile:
+        datatype = 'HitsOnTrack'
+    else:
+        print 'ERROR: could not understand data type (HitsOnTrack or EveryHit)'
+        sys.exit(0)
+    flatDetector = flatten(detector)
     tFile = r.TFile(inputFile, 'read')
     t = tFile.Get(tracker+'HitEfficiencyTuple/TrackMonTuple')
 
@@ -40,8 +49,8 @@ if __name__ == '__main__':
         os.system('mkdir ../Out/ResidualVSMomentum')
     hPositive, hNegative = {}, {}
     for sector in flatDetector:
-        hPositive[sector] = r.TH2F(sector+'-pos', sector + ' |Residual| VS Momentum', 50, 3000, 210000, 20, -0.4, 0.4)    #sector+'-positive', sector+'-positive')
-        hNegative[sector] = r.TH2F(sector+'-neg', sector + ' |Residual| VS Momentum', 50, 3000, 210000, 20, -0.4, 0.4)    #sector+'-negative', sector+'-negative')
+        hPositive[sector] = r.TGraph(); hPositive[sector].SetName(sector+'-pos'); hPositive[sector].SetTitle(sector+' |Residual| VS Momentum (charge = +1)')
+        hNegative[sector] = r.TGraph(); hNegative[sector].SetName(sector+'-neg'); hNegative[sector].SetTitle(sector+' |Residual| VS Momentum (charge = -1)')
     for track in t:
         if track.__getattr__('num'+tracker+'hits') > 0:
             ids = track.clusterSTchanID
@@ -51,27 +60,39 @@ if __name__ == '__main__':
                 sys.exit(0)
             for (i, res) in enumerate(residuals):
                 if track.charge == -1:
-                    hNegative[get_sector(ids[i])].Fill(float(track.p), math.fabs(float(res)))
+                    hNegative[get_sector(ids[i])].SetPoint( hNegative[get_sector(ids[i])].GetN(), float(track.p/1000.), math.fabs(float(res)))
                 else:
-                    hPositive[get_sector(ids[i])].Fill(float(track.p), math.fabs(float(res)))
+                    hPositive[get_sector(ids[i])].SetPoint( hPositive[get_sector(ids[i])].GetN(), float(track.p/1000.), math.fabs(float(res)))
+    for sector in flatDetector:
+        hPositive[sector].GetXaxis().SetTitle('Track momentum [GeV]'); hPositive[sector].GetYaxis().SetTitle('Hit residuals (absolute values) [mm]')
+        hNegative[sector].GetXaxis().SetTitle('Track momentum [GeV]'); hPositive[sector].GetYaxis().SetTitle('Hit residuals (absolute values) [mm]')
 
     # Look at the overlaps
     hOverlaps = {}
     for layer in detector.keys():
-        hOverlaps[layer] = {   'HitXVSTrackY':     r.TH2F( layer+'_HitXVSTrackY'    , layer+' HitXVSTrackY'    , 100, -800., 800., 100, -800., 800. ),
-                               'TrackXVSTrackY':   r.TH2F( layer+'_TrackXVSTrackY'  , layer+' TrackXVSTrackY'  , 100, -800., 800., 100, -800., 800. ),
-                               'ResidualVSTrackY': r.TH2F( layer+'_ResidualVSTrackY', layer+' ResidualVSTrackY', 40,   -0.4,  0.4, 100, -800., 800. )   }
+        hOverlaps[layer] = {   'HitXVSTrackY':     r.TGraph(),
+                               'TrackXVSTrackY':   r.TGraph(),
+                               'ResidualVSTrackY': r.TGraph()   }
     for track in t:
         for layer in detector.keys():
             if track.__getattr__(layer+'_nHits') > 1:
-                tsy = track.closestTrackState_y
-                tsx = track.closestTrackState_x
+                if datatype == 'EveryHit':
+                    tsy = track.closestTrackState_y
+                    tsx = track.closestTrackState_x
+                elif datatype == 'HitsOnTrack':
+                    tsy = track.trackState_y
+                    tsx = track.trackState_x
                 residuals = track.hit_residual
                 for (i, cx) in enumerate(track.cluster_x):
-                    hOverlaps[layer]['HitXVSTrackY'].Fill(    float(cx),           float(tsy[i]))
-                    hOverlaps[layer]['TrackXVSTrackY'].Fill(  float(tsx[i]),       float(tsy[i]))
-                    hOverlaps[layer]['ResidualVSTrackY'].Fill(float(residuals[i]), float(tsy[i]))
-
+                    hOverlaps[layer]['HitXVSTrackY'].SetPoint(     hOverlaps[layer]['HitXVSTrackY'].GetN()    , float(cx),           float(tsy[i]) )
+                    hOverlaps[layer]['TrackXVSTrackY'].SetPoint(   hOverlaps[layer]['TrackXVSTrackY'].GetN()  , float(tsx[i]),       float(tsy[i]) )
+                    hOverlaps[layer]['ResidualVSTrackY'].SetPoint( hOverlaps[layer]['ResidualVSTrackY'].GetN(), float(residuals[i]), float(tsy[i]) )
+    for layer in detector.keys():
+        for gr in hOverlaps[layer].keys():
+            hOverlaps[layer][gr].GetYaxis().SetTitle('Track Y extrapolation [mm]')
+        hOverlaps[layer]['HitXVSTrackY'].GetXaxis().SetTitle('Cluster X position [mm]')
+        hOverlaps[layer]['TrackXVSTrackY'].GetXaxis().SetTitle('Track X extrapolation [mm]')
+        hOverlaps[layer]['ResidualVSTrackY'].GetXaxis().SetTitle('Hit Residual [mm]')
 
 
 
