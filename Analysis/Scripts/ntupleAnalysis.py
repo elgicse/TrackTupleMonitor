b23d1cafe12cbaa7b51cf8635da86447f6cfdfce
elena <elena.graverini@gmail.com>
2014-12-10 16:48:43 +0100

    saving plots

--- Analysis/Scripts/ntupleAnalysis.py
+++ Analysis/Scripts/ntupleAnalysis.py
@@ -32,6 +32,8 @@ if __name__ == '__main__':
         print 'Sample usage: python ntupleAnalysis.py TT ../RootFiles/EveryHit/runs131973-133785-end2012.root'
         print 'Exiting now...'
         sys.exit(0)
+    if not os.path.exists('../Out/%s'%tracker):
+        os.system('mkdir ../Out/%s'%tracker)
     inputFile = str(sys.argv[2])
     if 'EveryHit' in inputFile:
         datatype = 'EveryHit'
@@ -40,13 +42,15 @@ if __name__ == '__main__':
     else:
         print 'ERROR: could not understand data type (HitsOnTrack or EveryHit)'
         sys.exit(0)
+    if not os.path.exists('../Out/%s/%s'%(tracker, datatype)):
+        os.system('mkdir ../Out/%s/%s'%(tracker, datatype))
     flatDetector = flatten(detector)
     tFile = r.TFile(inputFile, 'read')
     t = tFile.Get(tracker+'HitEfficiencyTuple/TrackMonTuple')
 
     # Looking into possible magnetic field effects
-    if not os.path.exists('../Out/ResidualVSMomentum'):
-        os.system('mkdir ../Out/ResidualVSMomentum')
+    if not os.path.exists('../Out/%s/%s/ResidualVSMomentum'%(tracker, datatype)):
+        os.system('mkdir ../Out/%s/%s/ResidualVSMomentum'%(tracker, datatype))
     hPositive, hNegative = {}, {}
     for sector in flatDetector:
         hPositive[sector] = r.TGraph(); hPositive[sector].SetName(sector+'-pos'); hPositive[sector].SetTitle(sector+' |Residual| VS Momentum (charge = +1)')
@@ -68,6 +72,8 @@ if __name__ == '__main__':
         hNegative[sector].GetXaxis().SetTitle('Track momentum [GeV]'); hPositive[sector].GetYaxis().SetTitle('Hit residuals (absolute values) [mm]')
 
     # Look at the overlaps
+    if not os.path.exists('../Out/%s/%s/Overlaps'%(tracker, datatype)):
+        os.system('mkdir ../Out/%s/%s/Overlaps'%(tracker, datatype))
     hOverlaps = {}
     for layer in detector.keys():
         hOverlaps[layer] = {   'HitXVSTrackY':     r.TGraph(),
@@ -89,10 +95,19 @@ if __name__ == '__main__':
                     hOverlaps[layer]['ResidualVSTrackY'].SetPoint( hOverlaps[layer]['ResidualVSTrackY'].GetN(), float(residuals[i]), float(tsy[i]) )
     for layer in detector.keys():
         for gr in hOverlaps[layer].keys():
+            hOverlaps[layer][gr].SetName(layer+'_'+gr); hOverlaps[layer][gr].SetTitle(layer+' '+gr)
             hOverlaps[layer][gr].GetYaxis().SetTitle('Track Y extrapolation [mm]')
         hOverlaps[layer]['HitXVSTrackY'].GetXaxis().SetTitle('Cluster X position [mm]')
         hOverlaps[layer]['TrackXVSTrackY'].GetXaxis().SetTitle('Track X extrapolation [mm]')
         hOverlaps[layer]['ResidualVSTrackY'].GetXaxis().SetTitle('Hit Residual [mm]')
-
+    for layer in detector.keys():
+        outfile = r.TFile('../Out/%s/%s/Overlaps/%s.root'%(tracker, datatype, layer), 'recreate')
+        for gr in hOverlaps[layer].keys():
+            hOverlaps[layer][gr].Write()
+            c1 = r.TCanvas(hOverlaps[layer][gr].GetName(), hOverlaps[layer][gr].GetTitle(), 1600, 1000)
+            hOverlaps[layer][gr].Draw('ap'); c1.Modified(); c1.Update()
+            c1.SaveAs('../Out/%s/%s/Overlaps/%s'%(tracker, datatype, layer) + '-%s.pdf'%gr)
+            c1.Write(); c1.Close()
+        outfile.Close()
 
 
