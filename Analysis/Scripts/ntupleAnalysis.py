c122dee8ce86b8a11682cf53761c5f1cd0234ef5
elena <elena.graverini@gmail.com>
2015-01-05 17:33:24 +0100

    now analysing also cluster size by sector

--- Analysis/Scripts/ntupleAnalysis.py
+++ Analysis/Scripts/ntupleAnalysis.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         detector = CreateDetectors.create_IT()
     else:
         print 'ERROR: please select tracker (IT or TT).'
-        print 'Sample usage: python -i ntupleAnalysis.py TT ../RootFiles/EveryHit/all2012-muEstimate.root save'
+        print 'Sample usage: python -i ntupleAnalysis.py TT ../RootFiles/EveryHit/all2012-muEstimate-Edges-2cm.root save'
         print 'Exiting now...'
         sys.exit(0)
     inputFile = str(sys.argv[2])
@@ -76,7 +76,8 @@ if __name__ == '__main__':
     flatDetector = flatten(detector)
     tFile = r.TFile(inputFile, 'read')
     t = tFile.Get(tracker+'HitEfficiencyTuple/TrackMonTuple')
-    
+
+
     # Looking into possible magnetic field effects
     if not os.path.exists('../Out/'+shortFilename+'/%s/%s/ResidualVSMomentum'%(tracker, datatype)):
         os.system('mkdir ../Out/'+shortFilename+'/%s/%s/ResidualVSMomentum'%(tracker, datatype))
@@ -112,6 +113,8 @@ if __name__ == '__main__':
                 c1.SaveAs('../Out/'+shortFilename+'/%s/%s/ResidualVSMomentum/%s.pdf'%(tracker, datatype, collection[gr].GetName()))
                 c1.Close()
     
+
+
     # Look at the overlaps in the single half-modules
     if tracker == 'IT':
         print 'Sorry I cannot do this for the IT, yet...'
@@ -301,4 +304,41 @@ if __name__ == '__main__':
             c.SaveAs('../Out/'+shortFilename+'/%s/%s/Overlaps/'%(tracker, datatype) + '%s.pdf'%c.GetName())
             c.Write(); c.Close()
         outfile.Close()
-    
+
+    # Looking into the cluster size distribution
+    if not os.path.exists('../Out/'+shortFilename+'/%s/%s/ClusterSizeBySector'%(tracker, datatype)):
+        os.system('mkdir ../Out/'+shortFilename+'/%s/%s/ClusterSizeBySector'%(tracker, datatype))
+    hCSize = {}
+    for sector in flatDetector:
+        hCSize[sector] = r.TH1I(sector, sector+' Cluster Size', 4, 0.5, 4.5)
+    for track in t:
+        if track.__getattr__('num'+tracker+'hits') > 0:
+            ids = track.clusterSTchanID
+            csize = track.clusterSize
+            if len(ids) != len(csize):
+                print "ERROR: number of entries mismatch"
+                sys.exit(0)
+            for (i, cs) in enumerate(csize):
+                hCSize[get_sector(ids[i])].Fill(int(cs))
+    for sector in flatDetector:
+        hCSize[sector].GetXaxis().SetTitle('Cluster size')
+        if save:
+            c1 = r.TCanvas(hCSize[sector].GetName(), hCSize[sector].GetTitle())
+            hCSize[sector].Draw('cp'); c1.Modified(); c1.Update()
+            label = r.TPaveText( 0.60 - r.gStyle.GetPadRightMargin(), 0.87 - r.gStyle.GetPadTopMargin(),
+                                 0.95 - r.gStyle.GetPadRightMargin(), 0.95 - r.gStyle.GetPadTopMargin(), "BRNDC");
+            label.SetFillColor(0); label.SetTextAlign(12); label.SetBorderSize(0)
+            label.SetTextFont(132); label.SetTextSize(0.06)
+            label.AddText(sector); label.Draw('same'); c1.Modified(); c1.Update()
+            mean = r.TPaveText( 0.60 - r.gStyle.GetPadRightMargin(), 0.80 - r.gStyle.GetPadTopMargin(),
+                                 0.95 - r.gStyle.GetPadRightMargin(), 0.88 - r.gStyle.GetPadTopMargin(), "BRNDC");
+            mean.SetFillColor(0); mean.SetTextAlign(32); mean.SetBorderSize(0)
+            mean.SetTextFont(132); mean.SetTextSize(0.06)
+            mean.AddText('Mean: %.2f'%hCSize[sector].GetMean()); mean.Draw('same'); c1.Modified(); c1.Update()
+            rms = r.TPaveText( 0.60 - r.gStyle.GetPadRightMargin(), 0.73 - r.gStyle.GetPadTopMargin(),
+                                 0.95 - r.gStyle.GetPadRightMargin(), 0.81 - r.gStyle.GetPadTopMargin(), "BRNDC");
+            rms.SetFillColor(0); rms.SetTextAlign(32); rms.SetBorderSize(0)
+            rms.SetTextFont(132); rms.SetTextSize(0.06)
+            rms.AddText('RMS: %.2f'%hCSize[sector].GetRMS()); rms.Draw('same'); c1.Modified(); c1.Update()
+            c1.SaveAs('../Out/'+shortFilename+'/%s/%s/ClusterSizeBySector/%s.pdf'%(tracker, datatype, hCSize[sector].GetName()))
+            c1.Close()
