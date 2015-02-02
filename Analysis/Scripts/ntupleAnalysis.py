2c7c9c6e9994517f1dc56a564135c5c60b8836b8
elena <elena.graverini@gmail.com>
2015-02-02 11:06:17 +0100

    positions half modules

--- Analysis/Scripts/ntupleAnalysis.py
+++ Analysis/Scripts/ntupleAnalysis.py
@@ -6,8 +6,8 @@ from conf import CreateDetectors
 from conf.STChannelID import *
 from conf.TTModules import *
 
-studies = ['MagField', 'OverlapsHalfModules', 'Overlaps', 'ClusterSize', 'FullLayers', 'Outliers']
-studies = ['Outliers']
+studies = ['MagField', 'OverlapsHalfModules', 'Overlaps', 'ClusterSize', 'FullLayers', 'Outliers', 'PositionsHalfModules']
+studies = ['PositionsHalfModules']
 
 cSaver = []
 r.gROOT.ProcessLine('.x tools/lhcbStyle.C')
@@ -545,17 +545,17 @@ if __name__ == '__main__':
             outliers['DeltaY'][hm]['nOutliers'] = hnum
             outliers['DeltaY'][hm]['ClusterXY'] = r.TGraph(len(cx), array('f',cx), array('f',cy))
             outliers['DeltaY'][hm]['TrackXY'] = r.TGraph(len(tsx), array('f',tsx), array('f',tsy))
-            outliers['DeltaY'][hm]['ClusterZ'] = r.TH1F('CZForYOutliers_%s'%sector,'CZForYOutliers_%s'%sector,10,min(cz),max(cz))
+            outliers['DeltaY'][hm]['ClusterZ'] = r.TH1F('CZForYOutliers_%s'%hm,'CZForYOutliers_%s'%hm,10,min(cz),max(cz))
             [ outliers['DeltaY'][hm]['ClusterZ'].Fill(z) for z in cz ]
-            outliers['DeltaY'][hm]['ClusterX'] = r.TH1F('CXForYOutliers_%s'%sector,'CXForYOutliers_%s'%sector,10,min(cx),max(cx))
+            outliers['DeltaY'][hm]['ClusterX'] = r.TH1F('CXForYOutliers_%s'%hm,'CXForYOutliers_%s'%hm,10,min(cx),max(cx))
             [ outliers['DeltaY'][hm]['ClusterX'].Fill(x) for x in cx ]
-            outliers['DeltaY'][hm]['ClusterY'] = r.TH1F('CYForYOutliers_%s'%sector,'CYForYOutliers_%s'%sector,10,min(cy),max(cy))
+            outliers['DeltaY'][hm]['ClusterY'] = r.TH1F('CYForYOutliers_%s'%hm,'CYForYOutliers_%s'%hm,10,min(cy),max(cy))
             [ outliers['DeltaY'][hm]['ClusterY'].Fill(y) for y in cy ]
-            outliers['DeltaY'][hm]['TrackZ'] = r.TH1F('TSZForYOutliers_%s'%sector,'TSZForYOutliers_%s'%sector,10,min(tsz),max(tsz))
+            outliers['DeltaY'][hm]['TrackZ'] = r.TH1F('TSZForYOutliers_%s'%hm,'TSZForYOutliers_%s'%hm,10,min(tsz),max(tsz))
             [ outliers['DeltaY'][hm]['TrackZ'].Fill(z) for z in tsz ]
-            outliers['DeltaY'][hm]['TrackX'] = r.TH1F('TSXForYOutliers_%s'%sector,'TSXForYOutliers_%s'%sector,10,min(tsx),max(tsx))
+            outliers['DeltaY'][hm]['TrackX'] = r.TH1F('TSXForYOutliers_%s'%hm,'TSXForYOutliers_%s'%hm,10,min(tsx),max(tsx))
             [ outliers['DeltaY'][hm]['TrackX'].Fill(x) for x in tsx ]
-            outliers['DeltaY'][hm]['TrackY'] = r.TH1F('TSYForYOutliers_%s'%sector,'TSYForYOutliers_%s'%sector,10,min(tsy),max(tsy))
+            outliers['DeltaY'][hm]['TrackY'] = r.TH1F('TSYForYOutliers_%s'%hm,'TSYForYOutliers_%s'%hm,10,min(tsy),max(tsy))
             [ outliers['DeltaY'][hm]['TrackY'].Fill(y) for y in tsy ]
             outliers['DeltaY'][hm]['ClusterXY'].SetMarkerColor(colors[i]), outliers['DeltaY'][hm]['ClusterXY'].SetTitle(hm)
             outliers['DeltaY'][hm]['TrackXY'].SetMarkerColor(colors[i]), outliers['DeltaY'][hm]['TrackXY'].SetTitle(hm)
@@ -569,11 +569,93 @@ if __name__ == '__main__':
             foutliers = r.TFile('../Out/'+shortFilename+'/%s/%s/Histograms/DeltaY.root'%(tracker, datatype), 'recreate')
             for hm in outliers['DeltaY']:
                 for hist in outliers['DeltaY'][hm].values():
-                    try:
-                        print hist.GetName(), type(hist)
-                    except AttributeError:
-                        pass
                     if isinstance(hist, r.TH1F) or isinstance(hist, r.TH1I):
                         hist.Write()
             foutliers.Close()
 
+    # Look at the position of hits in the single half-modules
+    if 'PositionsHalfModules' in studies:
+        if tracker == 'IT':
+            print 'Sorry I cannot do this for the IT, yet...'
+            sys.exit(0)
+        if not os.path.exists('../Out/'+shortFilename+'/%s/%s/Histograms'%(tracker, datatype)):
+            os.system('mkdir ../Out/'+shortFilename+'/%s/%s/Histograms'%(tracker, datatype))
+        if not os.path.exists('../Out/'+shortFilename+'/%s/%s/PlotsByHalfModule'%(tracker, datatype)):
+            os.system('mkdir ../Out/'+shortFilename+'/%s/%s/PlotsByHalfModule'%(tracker, datatype))
+        listOfHM = listOfTTHalfModules()
+        histosByHM = {}
+        for hm in listOfHM:
+            histosByHM[hm] = {  'HitXVSTrackY':     r.TGraph(),
+                                'TrackXVSTrackY':   r.TGraph(),
+                                'DeltaXVSTrackY':   r.TGraph(),
+                                'DeltaYVSTrackY':   r.TGraph(),
+                                'DeltaZVSTrackY':   r.TGraph(),
+                                'ResidualVSTrackY': r.TGraph(),
+                                'cx':  [], 'cy':  [], 'cz':  [],
+                                'tsx': [], 'tsy': [], 'tsz': []   }
+        for track in t:
+            if datatype == 'EveryHit':
+                tsx = track.closestTrackState_x
+                tsy = track.closestTrackState_y
+                tsz = track.closestTrackState_z
+            elif datatype == 'HitsOnTrack':
+                tsx = track.trackState_x
+                tsy = track.trackState_y
+                tsz = track.trackState_z
+            ids = track.clusterSTchanID
+            residuals = track.hit_residual
+            cx = track.cluster_x
+            cy = track.cluster_y
+            cz = track.cluster_z
+            histosByHM[hm]['cx'].extend(cx)
+            histosByHM[hm]['cy'].extend(cy)
+            histosByHM[hm]['cz'].extend(cz)
+            histosByHM[hm]['tsx'].extend(tsx)
+            histosByHM[hm]['tsy'].extend(tsy)
+            histosByHM[hm]['tsz'].extend(tsz)
+            for (i, id) in enumerate(ids):
+                hm = get_half_module(id)
+                histosByHM[hm]['HitXVSTrackY'].SetPoint(     histosByHM[hm]['HitXVSTrackY'].GetN()    , float(cx[i]),        float(tsy[i]) )
+                histosByHM[hm]['TrackXVSTrackY'].SetPoint(   histosByHM[hm]['TrackXVSTrackY'].GetN()  , float(tsx[i]),       float(tsy[i]) )
+                histosByHM[hm]['DeltaXVSTrackY'].SetPoint(   histosByHM[hm]['DeltaXVSTrackY'].GetN()  , float(tsx[i])-float(cx[i]),
+                                                                                                                               float(tsy[i]) )
+                histosByHM[hm]['DeltaYVSTrackY'].SetPoint(   histosByHM[hm]['DeltaYVSTrackY'].GetN()  , float(tsy[i])-float(cy[i]),
+                                                                                                                               float(tsy[i]) )
+                histosByHM[hm]['DeltaZVSTrackY'].SetPoint(   histosByHM[hm]['DeltaZVSTrackY'].GetN()  , float(tsz[i])-float(cz[i]),
+                                                                                                                               float(tsy[i]) )
+                histosByHM[hm]['ResidualVSTrackY'].SetPoint( histosByHM[hm]['ResidualVSTrackY'].GetN(), float(residuals[i]), float(tsy[i]) )
+        for hm in listOfHM:
+            for gr in histosByHM[hm].keys():
+                if isinstance(histosByHM[hm][gr], r.TGraph):
+                    histosByHM[hm][gr].SetName(hm+'_'+gr); histosByHM[hm][gr].SetTitle(hm+' '+gr)
+                    histosByHM[hm][gr].GetYaxis().SetTitle('Track Y extrapolation [mm]')
+            histosByHM[hm]['HitXVSTrackY'].GetXaxis().SetTitle('Cluster X position [mm]')
+            histosByHM[hm]['TrackXVSTrackY'].GetXaxis().SetTitle('Track X extrapolation [mm]')
+            histosByHM[hm]['DeltaXVSTrackY'].GetXaxis().SetTitle('(Track X extrapolation - cluster X position) [mm]')
+            histosByHM[hm]['DeltaYVSTrackY'].GetXaxis().SetTitle('(Track Y extrapolation - cluster Y position) [mm]')
+            histosByHM[hm]['DeltaZVSTrackY'].GetXaxis().SetTitle('(Track state Z - cluster Z position) [mm]')
+            histosByHM[hm]['ResidualVSTrackY'].GetXaxis().SetTitle('Hit Residual [mm]')
+        if save:
+            outfile = r.TFile('../Out/'+shortFilename+'/%s/%s/Histograms/PositionsByHM.root'%(tracker, datatype), 'recreate')
+            for hm in listOfHM:
+                for gr in histosByHM[hm].keys():
+                    if isinstance(histosByHM[hm][gr], r.TGraph):
+                        #histosByHM[hm][gr].Write()
+                        c1 = r.TCanvas(histosByHM[hm][gr].GetName(), histosByHM[hm][gr].GetTitle(), 1600, 1000)
+                        histosByHM[hm][gr].Draw('ap'); c1.Modified(); c1.Update()
+                        # Add label with half module name
+                        label = r.TPaveText( 0.60 - r.gStyle.GetPadRightMargin(), 0.87 - r.gStyle.GetPadTopMargin(),
+                                             0.95 - r.gStyle.GetPadRightMargin(), 0.95 - r.gStyle.GetPadTopMargin(), "BRNDC");
+                        label.SetFillColor(0); label.SetTextAlign(12); label.SetBorderSize(0)
+                        label.SetTextFont(132); label.SetTextSize(0.06)
+                        label.AddText(hm); label.Draw('same'); c1.Modified(); c1.Update()
+                        c1.SaveAs('../Out/'+shortFilename+'/%s/%s/PlotsByHalfModule/%s'%(tracker, datatype, hm) + '-%s.png'%gr)
+                        #c1.Write()
+                        c1.Close()
+                    elif isinstance(histosByHM[hm][gr], list):
+                        if len(histosByHM[hm][gr])>0: # skip empty modules
+                            h = r.TH1F('%s_%s'%(gr, hm), '%s_%s'%(gr, hm), 100, min(histosByHM[hm][gr]), max(histosByHM[hm][gr]))
+                            [ h.Fill(x) for x in histosByHM[hm][gr] ]
+                            h.Write()
+                            h.Delete()
+            outfile.Close()
